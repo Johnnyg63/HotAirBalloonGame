f6507b10ccca77e62071bc9978fffc0c3cb3367d
John Galvin <jgalvin@backupearth.com>
2023-08-26 10:51:08 +0100

    Demo Project Setup completed

---
+++ HotAirBalloonGame/HotAirBalloon.cpp
@@ -0,0 +1,45 @@
+
+/*
+* Hot Air Ballon Game from 1983 by John Galvin, a walk down memory lane
+* I wrote this game in BASIC all those years ago for the C64
+* This is my attempt to get it running, somewhat the same, as it was in 1983
+
+*/
+
+#define OLC_PGE_APPLICATION
+#include "olcPixelGameEngine.h"
+
+// Override base class with your custom functionality
+class Example : public olc::PixelGameEngine
+{
+public:
+	Example()
+	{
+		// Name your application
+		sAppName = "Hot Air Balloon Johnnyg63";
+	}
+
+public:
+	bool OnUserCreate() override
+	{
+		// Called once at the start, so create things here
+		return true;
+	}
+
+	bool OnUserUpdate(float fElapsedTime) override
+	{
+		// Called once per frame, draws random coloured pixels
+		for (int x = 0; x < ScreenWidth(); x++)
+			for (int y = 0; y < ScreenHeight(); y++)
+				Draw(x, y, olc::Pixel(rand() % 256, rand() % 256, rand() % 256));
+		return true;
+	}
+};
+
+int main()
+{
+	Example demo;
+	if (demo.Construct(256, 240, 4, 4))
+		demo.Start();
+	return 0;
+}
