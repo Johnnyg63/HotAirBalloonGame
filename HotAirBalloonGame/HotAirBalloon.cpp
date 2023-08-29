4e2684d6af0b8e78fbbafd44f1b0749dd6dc23d3
John Galvin <jgalvin@backupearth.com>
2023-08-29 21:27:12 +0100

    Creating Transform view and Camera

--- HotAirBalloonGame/HotAirBalloon.cpp
+++ HotAirBalloonGame/HotAirBalloon.cpp
@@ -9,6 +9,12 @@
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
 
+#define OLC_PGEX_TRANSFORMEDVIEW
+#include "olcPGEX_TransformedView.h"
+
+#include "olcUTIL_Camera2D.h"
+
+
 
 // Override base class with your custom functionality
 class Example : public olc::PixelGameEngine
@@ -35,7 +41,6 @@ public:
 
 	/* C64 Colour Code Pixels */
 	// Thank you too: https://lospec.com/palette-list/commodore64 
-
 	// Holds the correct RGB Colours codes for C64
 	struct C64Color
 	{
@@ -62,12 +67,18 @@ public:
 		
 	};
 
-	// C64 colour palette
-	C64Color C64Color;
-	
+	C64Color C64Color; // C64 colour palette
+
+
+	/* Game Vars */
+	bool bGameLoaded = false;
+	bool bGameLoading = false;
+	bool bStartRec = false;
+
+	// C64 Display Stuff
+private:
 
 	/* C64 Base Screen text */
-
 	std::string strHeaderText1 = "**** COMMODARE 64 BASIC V2 ****";
 	std::string strLineBreak = " ";
 	std::string strHeaderText2 = "64K RAM SYSTEM 38911 BASIC BYTES FREE";
@@ -75,63 +86,7 @@ public:
 	std::string strLoad = "LOAD";
 	std::string strPlay = "PRESS PLAY ON TAPE";
 	std::string strLoading = "LOADING...";
-	std::string strRun = "RUN...";
-
-	/* Game Vars */
-	bool bGameLoaded = false;
-	bool bGameLoading = false;
-	bool bStartRec = false;
-
-
-
-public:
-	bool OnUserCreate() override
-	{
-		// Lets build up our colour code (will make life easier later
-		C64LoadColourCodes();
-
-		C64CreateBalloonSprite();
-
-		LoadLevel(1);
-
-		return true;
-	}
-
-	bool OnUserUpdate(float fElapsedTime) override
-	{
-		// Called once per frame, draws random coloured pixels
-		SetDrawTarget(nullptr);
-		
-		if (GetKey(olc::SPACE).bPressed) bStartRec = true;
-
-		if (!bStartRec) return true;
-
-		
-		if (!bGameLoaded)
-		{
-			FillRectDecal({ 0,0 }, { (float)ScreenWidth(), (float)ScreenHeight() }, C64Color.LightBlue);
-			// We display the C64 screen and loading screen until the 'game' is loaded
-			if (bGameLoading) C64LoadingScreen();
-			C64DisplayScreen();
-			C64DisplayHeader(fElapsedTime);
-			
-			return true;
-		}
-
-		FillRectDecal({ 0,0 }, { (float)ScreenWidth(), (float)ScreenHeight() }, C64Color.Blue);
-		
-		// Lets get the party going
-		// first up we need a background
-		DrawDecal({ 0, 40 }, decC64Level);
-		
-		if (GetMouse(0).bHeld) DrawDecal(GetMousePos(), decBalloon);
-
-
-		return true;
-	}
-
-
-private:
+	std::string strRun = "RUN";
 
 	float fDisplayTime = 0.0f;
 
@@ -162,7 +117,7 @@ private:
 
 	void C64DisplayScreen()
 	{
-		
+
 		FillRectDecal({ 20,20 }, { 280, 160 }, C64Color.Blue);
 
 	}
@@ -171,8 +126,8 @@ private:
 	{
 		fDisplayTime += fElapsedTime;
 		bGameLoading = false;
-		DrawStringPropDecal({ 45, 30 }, strHeaderText1, C64Color.LightBlue, {1.0f, 0.8f});
-		DrawStringPropDecal({ 30, 40 }, strHeaderText2, C64Color.LightBlue, {1.0f, 0.8f});
+		DrawStringPropDecal({ 45, 30 }, strHeaderText1, C64Color.LightBlue, { 1.0f, 0.8f });
+		DrawStringPropDecal({ 30, 40 }, strHeaderText2, C64Color.LightBlue, { 1.0f, 0.8f });
 		DrawStringPropDecal({ 22, 60 }, strReady, C64Color.LightBlue, { 1.0f, 0.8f });
 
 		if (fDisplayTime > 2.0f) DrawStringPropDecal({ 22, 70 }, strLoad, C64Color.LightBlue, { 1.0f, 0.8f });
@@ -206,7 +161,7 @@ private:
 	void C64CreateBalloonSprite()
 	{
 		// Right we have a sprite of size 24 X 20
-		
+
 		sprBalloon = new olc::Sprite(24, 20);
 
 
@@ -255,22 +210,205 @@ private:
 
 		decBalloon = new olc::Decal(sprBalloon);
 
-		
+
 
 	}
 
+	// Game Level Stuff
 private:
 
 	void LoadLevel(int8_t nLevel)
 	{
 		// for this demo there will only be one level
 		// but add more as you wish
-		
+
 		// 1: We need the level graphics
 		// CopyRight: https://www.spriters-resource.com/commodore_64/gianasisters30thanniversaryhack/sheet/199050/
 		sprC64Level = new olc::Sprite("./assests/C64LevelOne.png");
 		decC64Level = new olc::Decal(sprC64Level);
 	}
+
+	// Transform View Stuff
+public:
+
+	// Transformed view object to make world offsetting simple
+	olc::TileTransformedView tv;
+
+	// Conveninet constants to define tile map world
+	olc::vi2d m_vWorldSize = { 64, 10 }; // 2048 64 cells
+	olc::vi2d m_vTileSize = { 32, 20 };  
+
+	// The camera!
+	olc::utils::Camera2D camera;
+
+	// The point that represents the player, it is "tracked"
+	// by the camera
+	olc::vf2d vTrackedPoint;
+
+	// Flag whether we are in "free roam" or "play" mode
+	bool bFreeRoam = false;
+
+	// The world map, stored as a 1D array
+	std::vector<uint8_t> vWorldMap;
+
+	void TestCode(float fElapsedTime)
+	{
+		// In free roam, middle mouse button pans & zooms
+		if (bFreeRoam)
+			tv.HandlePanAndZoom();
+
+		// Handle player "physics" in response to key presses
+		olc::vf2d vVel = { 0.0f, 0.0f };
+		if (GetKey(olc::Key::W).bHeld || GetKey(olc::Key::UP).bHeld) vVel += {0, -1}; // up
+		if (GetKey(olc::Key::S).bHeld || GetKey(olc::Key::DOWN).bHeld) vVel += {0, +1}; // down
+		if (GetKey(olc::Key::A).bHeld || GetKey(olc::Key::LEFT).bHeld) vVel += {-1, 0}; // left
+		if (GetKey(olc::Key::D).bHeld || GetKey(olc::Key::RIGHT).bHeld) vVel += {+1, 0}; // right
+		vTrackedPoint += vVel * 4.0f * fElapsedTime;
+
+		// Some controlls
+		if (vTrackedPoint.x < 0.05f) vTrackedPoint.x = 0.05f;
+		if (vTrackedPoint.x > 63.0f) vTrackedPoint.x = 63.0f;
+
+		// Switch between "free roam" and "play" mode with TAB key
+		if (GetKey(olc::Key::TAB).bPressed)
+		{
+			// Always setup camera to play mode when tab key pressed
+			tv.SetWorldOffset(camera.GetViewPosition());
+			tv.SetWorldScale(m_vTileSize);
+			bFreeRoam = !bFreeRoam;
+		}
+
+		// Switch camera mode in operation
+		if (GetKey(olc::Key::K1).bPressed)
+			camera.SetMode(olc::utils::Camera2D::Mode::Simple);
+		if (GetKey(olc::Key::K2).bPressed)
+			camera.SetMode(olc::utils::Camera2D::Mode::EdgeMove);
+		if (GetKey(olc::Key::K3).bPressed)
+			camera.SetMode(olc::utils::Camera2D::Mode::LazyFollow);
+		if (GetKey(olc::Key::K4).bPressed)
+			camera.SetMode(olc::utils::Camera2D::Mode::FixedScreens);
+
+		// Update the camera, if teh tracked object remains visible, 
+		// true is returned
+		bool bOnScreen = camera.Update(fElapsedTime);
+
+		// In "play" mode, set the transformed view to that required by
+		// the camera
+		if (!bFreeRoam)
+			tv.SetWorldOffset(camera.GetViewPosition());
+
+		// Render "tile map", by getting visible tiles
+		olc::vi2d vTileTL = tv.GetTopLeftTile().max({ 0,0 });
+		olc::vi2d vTileBR = tv.GetBottomRightTile().min(m_vWorldSize);
+		olc::vi2d vTile;
+		// Then looping through them and drawing them
+		for (vTile.y = vTileTL.y; vTile.y < vTileBR.y; vTile.y++)
+			for (vTile.x = vTileTL.x; vTile.x < vTileBR.x; vTile.x++)
+			{
+				int idx = vTile.y * m_vWorldSize.x + vTile.x;
+
+				if (vWorldMap[idx] == 0)
+					tv.FillRectDecal(vTile, { 1.0f, 1.0f }, olc::Pixel(40, 40, 40));
+
+				if (vWorldMap[idx] == 1)
+					tv.FillRectDecal(vTile, { 1.0f, 1.0f }, olc::Pixel(60, 60, 60));
+			}
+
+		// Draw the "player" as a 1x1 cell
+		tv.DrawDecal(vTrackedPoint - olc::vf2d(0.5f, 0.5f), decBalloon);
+		olc::vi2d vGVPos = { (int32_t)camera.GetViewPosition().x, (int32_t)camera.GetViewPosition().y };
+		float posX = std::min((vTrackedPoint.x * 31), (decC64Level->sprite->Size().x - 320.0f));
+		DrawPartialDecal({ 0.0f, 40.0f }, { 320.0f, 160.0f }, decC64Level, { posX, 0}, {300.0f, 160.0f});
+		//tv.FillRectDecal(vTrackedPoint - olc::vf2d(0.5f, 0.5f), { 1.0f, 1.0f }, olc::BLUE);
+
+		// Overlay with information
+		if (bFreeRoam)
+		{
+			tv.FillRectDecal(camera.GetViewPosition(), camera.GetViewSize(), olc::PixelF(255.0f, 0.0f, 0.0f, 0.5f));
+			DrawStringPropDecal({ 2, 2 }, "TAB: Free Mode, M-Btn to Pan & Zoom", olc::YELLOW);
+		}
+		else
+			DrawStringPropDecal({ 2,2 }, "TAB: Play Mode", olc::YELLOW);
+
+		DrawStringPropDecal({ 2,12 }, "WASD  : Move", olc::YELLOW);
+		DrawStringPropDecal({ 2,22 }, "CAMERA: 1) Simple  2) EdgeMove  3) LazyFollow  4) Screens", olc::YELLOW);
+		DrawStringPropDecal({ 2,42 }, vTrackedPoint.str(), olc::YELLOW);
+	}
+
+public:
+	bool OnUserCreate() override
+	{
+		// Lets build up our colour code (will make life easier later
+		C64LoadColourCodes();
+
+		C64CreateBalloonSprite();
+
+		LoadLevel(1);
+
+		// Construct transform view
+		tv = olc::TileTransformedView(GetScreenSize(), m_vTileSize);
+
+		// Construct Camera
+		vTrackedPoint = { 1.0f, 5.0f };
+		camera = olc::utils::Camera2D(GetScreenSize() / m_vTileSize, vTrackedPoint);
+
+		// Configure Camera
+		camera.SetTarget(vTrackedPoint);
+		camera.SetMode(olc::utils::Camera2D::Mode::LazyFollow);
+		camera.SetWorldBoundary({ 0.0f, 0.0f }, m_vWorldSize);
+		camera.EnableWorldBoundary(true);
+
+		// Create "tile map" world with just two tiles
+		size_t mapSize = m_vWorldSize.x * m_vWorldSize.y;
+		vWorldMap.resize(mapSize);
+
+		// TODO: Add load code here
+		for (int i = 0; i < vWorldMap.size(); i++)
+			vWorldMap[i] = ((rand() % 20) == 1) ? 1 : 0;
+
+		// Set background colour
+		Clear(C64Color.Blue);
+
+		return true;
+	}
+
+	bool OnUserUpdate(float fElapsedTime) override
+	{
+		// Called once per frame, draws random coloured pixels
+		SetDrawTarget(nullptr);
+		
+		//if (GetKey(olc::SPACE).bPressed) bStartRec = true;
+
+		//if (!bStartRec) return true;
+
+		//
+		//if (!bGameLoaded)
+		//{
+		//	FillRectDecal({ 0,0 }, { (float)ScreenWidth(), (float)ScreenHeight() }, C64Color.LightBlue);
+		//	// We display the C64 screen and loading screen until the 'game' is loaded
+		//	if (bGameLoading) C64LoadingScreen();
+		//	C64DisplayScreen();
+		//	C64DisplayHeader(fElapsedTime);
+		//	
+		//	return true;
+		//}
+
+		//FillRectDecal({ 0,0 }, { (float)ScreenWidth(), (float)ScreenHeight() }, C64Color.Blue);
+		//
+		//// Lets get the party going
+		//// first up we need a background
+		//DrawDecal({ 0, 40 }, decC64Level);
+		//
+		//if (GetMouse(0).bHeld) DrawDecal(GetMousePos(), decBalloon);
+
+		TestCode(fElapsedTime);
+
+
+		return true;
+	}
+
+
+
 };
 
 int main()
